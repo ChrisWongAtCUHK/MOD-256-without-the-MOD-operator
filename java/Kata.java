74479ae1a6a5c5dacea9dfec97eabec92e37c254
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-22 11:03:31 +0800

    My ally solution.

--- java/Kata.java
+++ java/Kata.java
@@ -1,7 +1,12 @@
-public class Kata
-{
-  public static int mod256WithoutMod(int number)
-  {
-    return number - (number/256 * 256);
-  }
+public class Kata {
+	public static int mod256WithoutMod(int number) {
+		if (Integer.signum(number) >= 0) { // positive number
+			number &= 0xff; // mod 256
+		} else {
+			number = ~(number - 1); // to positive
+			number &= 0xff; // mod 256
+			number = ~number + 1; // back to negative
+		}
+		return number;
+	}
 }
